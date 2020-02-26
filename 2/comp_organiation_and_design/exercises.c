15dbe63e0739ac31d58c01a2085d60268830808a
David Draguta <daviddraguta@gmail.com>
2020-02-26 18:26:12 -0500

    First recursive procedure translated to MIPS.

--- 2/comp_organiation_and_design/exercises.c
+++ 2/comp_organiation_and_design/exercises.c
@@ -44,3 +44,32 @@
  *    //jump to return address value stored in $ra.
  *    jr $ra  
  */
+
+/*  int fib (int n) 
+ *  {
+ *      if (n<1) return 1;
+ *      else return n * fib(n-1);
+ *  }
+ *
+ *  What is the compiled MIPS assembly code?
+ *  
+ *   fib:
+ *     addi $sp, $sp, -2*4 //pop 2 to stack
+ *     sw $ra, 1*4($sp)    //save address
+ *     sw $a0 0($sp)       //save argument
+ *     slti $t0, $a0, 1    //if $a0<1, $t0=1, else $t0=0;
+ *     beq $t0, $zero, L1  //if $t0=0, jump to L1
+ *     addi $v0, zero, 1   //$v0=1
+ *     addi $sp, $sp, 2*4  //$sp+=2*4
+ *     jr $ra              //jump to $ra
+ *  L1:
+ *     addi $a0, $a0, -1  //$a0 -=1 
+ *     jal fact           //$ra=PC+1*4; go to fact
+ *     lw $a0, 0($sp)     //load argument
+ *     lw $ra, 1*4($sp)   //load return address
+ *     addi $sp, $sp, 2*4 //pop 2 from stack
+ *     mul $v0, $a0, $v0  //$v0 = $a0 * $v0
+ *     jr $ra             //jump to $ra
+ */
+
+
