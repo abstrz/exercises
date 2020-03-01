e38657e1ab64a1d020c09346e1c17bdfbff7087d
David Draguta <daviddraguta@gmail.com>
2020-03-01 11:11:58 -0500

    Leetcode practice problems.

---
+++ misc/leetcode_practice/practice.py
@@ -0,0 +1,35 @@
+class Solution:
+    def sortLists(self, l1, l2):
+        def recursively(result, l1, l2):
+            if l1 == []:
+                result += l2
+                return result
+            elif l2 == []:
+                result += l1
+                return result
+            elif l1[0]<l2[0]:
+                result.append(l1[0])
+                return recursively(result, l1[1:], l2)
+            elif l2[0]<l1[0]:
+                result.append(l2[0])
+                return recursively(result, l1, l2[1:])
+            else:
+                result.append(l1[0])
+                result.append(l2[0])
+                return recursively(result, l1[1:], l2[1:])
+        return recursively([], l1, l2)
+
+    def findMedianSortedArrays(self, nums1, nums2):
+        sorted_nums = sortLists(nums1, nums2)
+        lngth = len(sorted_nums);
+        mid = int(lngth/2)
+        if (lngth % 2 == 1):
+            return sorted_nums[mid]
+        else:
+            return (sorted_nums[mid-1] + sorted_nums[mid])/2
+
+
+
+
+
+
