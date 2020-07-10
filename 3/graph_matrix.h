14b3d2048f64c36dcabc8a59b88377e17e0a6dd0
David Draguta <daviddraguta@gmail.com>
2020-07-09 22:28:18 -0400

    qweqweqe

--- 3/graph_matrix.h
+++ 3/graph_matrix.h
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+#include "arr.h"
 
 typedef int **graph_m; 
 
@@ -8,9 +9,11 @@ void rand_init();
 
 int dim(graph_m g);
 void printgraph_m(graph_m g);
-void insert_vertex(graph_m g); 
-void delete_vertex(int i, graph_m g); 
+void insert_vertex(graph_m g);
+void delete_vertex(int a, graph_m g);
+graph_m empty_graph_m();
 graph_m generate_graph_m(int n);
+graph_m generate_complete_graph_m(int n);
 
 int has_edge(int i, int j, graph_m g);
 void insert_edge(int i, int j, int w, graph_m g); //add a row and column of zeros.
@@ -18,4 +21,7 @@ void insert_edge_bothways(int i, int j, int w, graph_m g); //add a row and colum
 void delete_edge(int i, int j, graph_m g); 
 void delete_edge_bothways(int i, int j, graph_m g); //add a row and column of zeros.
 
-graph_m generate_complete_graph_m(int n);
+int check_cycle(graph_m soln);
+
+
+graph_m nearest_neighbor(graph_m g);
