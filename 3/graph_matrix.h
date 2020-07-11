32e0f1476aa68707b8108286409c6f3937c44218
David Draguta <daviddraguta@gmail.com>
2020-07-11 19:11:24 -0400

    Shwaqaqqaaaaaaaaaaaaaaaaaaaaa

--- 3/graph_matrix.h
+++ 3/graph_matrix.h
@@ -3,25 +3,26 @@
 #include <time.h>
 #include "arr.h"
 
-typedef int **graph_m; 
+typedef int_arr *graph_m; 
 
 void rand_init();
 
 int dim(graph_m g);
 void printgraph_m(graph_m g);
-void insert_vertex(graph_m g);
-void delete_vertex(int a, graph_m g);
-graph_m empty_graph_m();
-graph_m generate_graph_m(int n);
-graph_m generate_complete_graph_m(int n);
 
 int has_edge(int i, int j, graph_m g);
+int connected(int i, int j, graph_m g);
 void insert_edge(int i, int j, int w, graph_m g); //add a row and column of zeros.
 void insert_edge_bothways(int i, int j, int w, graph_m g); //add a row and column of zeros.
 void delete_edge(int i, int j, graph_m g); 
 void delete_edge_bothways(int i, int j, graph_m g); //add a row and column of zeros.
 
+int_arr *generate_blacklist(graph_m g);
+graph_m generate_graph_m(int n);
+graph_m generate_complete_graph_m(int n);
 int check_cycle(graph_m soln);
 
 
 graph_m nearest_neighbor(graph_m g);
+graph_m closest_pair(graph_m g);
+
